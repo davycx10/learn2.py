063186bce73642bda02fc9cec134770dceba28be
Dave Israël <dave.israel@oskey.io>
2024-10-25 12:12:14 +0200

    feat/def main()

--- src/lear2.py
+++ src/lear2.py
@@ -1,5 +1,36 @@
-day = [ "lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi", "dimanche" ]
-da = 5
-while da <= 6:
-    print(day[da])    
-    da = da + 1
+
+
+def main():
+    wallet = int(input("quel est la valeur de votre porte monnaie: "))
+    produit = 50
+    result = (wallet - produit)
+    print(f"après votre achat, voici la nouvelle valeur de votre porte monnaie {result}")
+    
+    #note1 = int(input("Donnez la première note "))
+    #note2 = int(input("Donnez la seconde note "))
+    #note3 = int(input("Donnez la derniere note "))
+    #result = int((note1 + note2 + note3) / 3)
+    #print(f" votre moyenne est de : {result:.2f}")
+#def moyenne(a, b ,c):
+#    return (a + b + c)/3
+#moyenne(13.75, 10, 12.50)
+#print(moyenne(13.75, 10, 12.50))
+    
+    
+    
+    # création d'une variable 'username' dans lequel on affecte la valeur Dave 
+    #username = "Dave"
+    #creation d'une variable 'age' dans lequel on affecte la valeur 19
+    #age = 19
+    #création d'une variable 'wallet' dans lequel on affecte la valeur 12.7
+    #wallet = 12.7
+    #print(f"Salut {username}, tu as {age}ans et il te reste {wallet}€ dans ton portefeuille")
+
+#    print("hello world")   
+    
+if __name__ == '__main__':
+   main()     
+    
+    
+    
+    
